71076b44cd8959a8782ccd6c9978a7e0309cd88a
yusuf <yusuf@gmail.com>
2022-12-09 21:39:07 +0300

    urunler sayfasında id düzenlemesi yapıldı

--- src/components/urunler.jsx
+++ src/components/urunler.jsx
@@ -4,35 +4,84 @@ import {
   Routes,
   Route,
   Link,
-  useParams
+  useLocation,
+  useParams,
 } from "react-router-dom";
 
+const veri=[
+  {id:0,
+   kategori:"anakart",
+   urun:"msı",
+  fiyat:100,
+  aciklama:"3 adet ddr5 bulunmaktadır"
+  },
+  {id:1,
+    kategori:"harddisk",
+    urun:"western digital",
+   fiyat:100,
+   aciklama:"500gb depolama alanı"
+   },
+   {id:2,
+    kategori:"SSD",
+    urun:"sandisk",
+   fiyat:100,
+   aciklama:"okuma yazma hızı yüksek"
+   }
+];
+
+
+
+
 function Urunler() {
+  
+  const location = useLocation();
+  
   return (
+    
     <div>
+   
        <div className='container mt-5'>
          <div className='row'>
             <div className='col-md-12'>
              <div className="alert alert-primary"  role="alert" >
                <div className="container">
                 <div className="row">
-                  <div className="col-md-6">
-                             <div class="card">
-                            <div class="card-body">
-                              <h5 class="card-title">Kategori</h5>
-                              <p class="card-text">Ürün</p>
-                              <Link to="#" className= "btn btn-primary">Detaya git</Link>
-                            </div>
-                          </div>
-                  </div>
+                  
+                  {veri.map((linkler)=>{
+                    const id=linkler.id
+                    return(
+                      <div className='col-md-6'>
+                     <div class="card" >
+                     <div class="card-body">
+                       <h5 class="card-title">{`${linkler.kategori}`}</h5>
+                       <p class="card-text">{`${linkler.kategori}`}</p>
+                       <Link to={`${location.pathname}/${id}`} className='btn btn-primary'>Detaylara git</Link>
+                     </div>
+                   </div>
+                   </div>
+                   )
+                  })}           
                 </div>
                </div>
             </div>  
         </div>
     </div>
        </div>
+
+    
+       
+
+
+
   </div>
+  
   )
 }
 
+
+
+
+
 export default Urunler
+
+
